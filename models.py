f57cc16c81a223cc6b96be772b6cb1474a9b81fc
Topasm <dhkdwk0215@naver.com>
2025-05-08 18:53:36 +0900

    Add evaluation and training scripts for ResNet action classification model

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import torchvision.models as models
+import time
 
 
 class ActionModel(nn.Module):
@@ -30,3 +32,59 @@ class ActionModel(nn.Module):
         x = self.features(x)
         x = self.classifier(x)
         return x
+
+
+class ResNetActionModel(nn.Module):
+    def __init__(self, num_classes=4, pretrained=True, resnet_type=18):
+        """
+        Initialize ResNet model for action classification
+
+        Args:
+            num_classes: Number of output classes
+            pretrained: Whether to use pretrained weights
+            resnet_type: ResNet architecture (18, 34, 50, 101, 152)
+        """
+        super(ResNetActionModel, self).__init__()
+
+        # Select ResNet architecture
+        if resnet_type == 18:
+            base_model = models.resnet18(pretrained=pretrained)
+        elif resnet_type == 34:
+            base_model = models.resnet34(pretrained=pretrained)
+        elif resnet_type == 50:
+            base_model = models.resnet50(pretrained=pretrained)
+        elif resnet_type == 101:
+            base_model = models.resnet101(pretrained=pretrained)
+        elif resnet_type == 152:
+            base_model = models.resnet152(pretrained=pretrained)
+        else:
+            raise ValueError(f"Unsupported ResNet type: {resnet_type}")
+
+        # Remove the final fully connected layer
+        self.features = nn.Sequential(*list(base_model.children())[:-1])
+
+        # Get the number of features from the last layer
+        if resnet_type in [18, 34]:
+            num_features = 512
+        else:
+            num_features = 2048
+
+        # Add custom classifier
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(num_features, num_classes)
+        )
+
+        # For inference speed measurement
+        self.last_inference_time = 0
+
+    def forward(self, x):
+        start_time = time.time()
+        x = self.features(x)
+        x = self.classifier(x)
+        self.last_inference_time = (time.time() - start_time) * 1000  # ms
+        return x
+
+    def get_inference_time(self):
+        """Returns the last inference time in milliseconds"""
+        return self.last_inference_time
